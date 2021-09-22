91a45c5fd22bfedd7dde5b0b57085a15bf39d884
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-09-22 12:31:20 +0300

    Java Egzersiz

---
+++ generics/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> sehirler = new ArrayList<String>();
+        sehirler.add("Ankara");
+        sehirler.add("Bursa");
+
+
+        MyList<Customer> sehir = new MyList<Customer>();
+        sehir.add(new Customer());
+
+        // Customer
+        // Product
+
+
+        // Validator
+
+
+    }
+}
