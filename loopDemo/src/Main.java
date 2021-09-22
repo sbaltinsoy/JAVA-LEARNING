8a18617e7fe2248a1cf139bef4683979557bbbf4
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-09-22 12:36:04 +0300

    Java Egzersiz

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    for(int i =1; i<10;i+=2){
+            System.out.println(i);
+        }
+
+        int sayac=2;
+        while(sayac <10){
+            System.out.println(sayac);
+            sayac++;
+        }
+        System.out.println(sayac + "son hal");
+
+        do{
+            System.out.println(sayac);
+            sayac++;
+        }while(sayac<10);
+
+    }
+}
